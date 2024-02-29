52b3f7647e754bec3bd3e090c85eb5e9e0eb5ee0
BIkram Baramn <150315437+bikram993298@users.noreply.github.com>
2024-02-29 23:42:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("button1");
+
+login1.onclick=function(){
+  window.location.href='mainpage.html';
+}
+
+ 
+
+
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
